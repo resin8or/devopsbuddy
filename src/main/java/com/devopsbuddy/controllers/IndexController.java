7866cc226e87582d693e0ca9d3908f7e4147793f
Jamie Tobin <jamie.tobin@gmail.com>
2017-02-09 23:20:19 +0000

    Added placeholder index page

--- src/main/java/com/devopsbuddy/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/controllers/IndexController.java
@@ -3,11 +3,13 @@ package com.devopsbuddy.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Created by jamietobin on 09/02/2017.
+ */
 @Controller
-public class HelloWorldController {
-
-	@RequestMapping("/")
-	public String sayHello(){
-		return "index";
-	}
+public class IndexController {
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
 }
